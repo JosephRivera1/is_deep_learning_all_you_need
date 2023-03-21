8f9c4d7a4acabe544f5b660ea4d79a11dec4896a
Mahad Ali <mahad852@gmail.com>
2023-03-20 20:56:37 -0400

    1. Tweaks to the tabnet model hyperparameters. 2. Code qualtiy improvements (moved future sales dataset loading function to a separate file).

--- tabnet.py
+++ tabnet.py
@@ -3,12 +3,10 @@ from pytorch_tabnet.tab_model import TabNetRegressor
  
 import os
 import torch
-import pandas as pd
-import numpy as np
 from sklearn.preprocessing import RobustScaler,StandardScaler
 import os
-from sklearn.model_selection import train_test_split 
 from sklearn.model_selection import KFold
+from LoadFutureSalesDataset import load_future_sales_dataset
 
 
 def save_model(model_name, model):
@@ -24,42 +22,34 @@ def save_model(model_name, model):
 
 def train(X_train, Y_train, X_val, Y_val):
     tb_reg = TabNetRegressor(
-        verbose=0,
         seed=42,
-        device_name = 'cuda' if torch.cuda.is_available() else 'cpu'
+        device_name = 'cuda' if torch.cuda.is_available() else 'cpu',
+        n_d=8, n_a=8, n_steps=3, gamma=1.3,
+        lambda_sparse=0, optimizer_fn=torch.optim.Adam,
+        optimizer_params=dict(lr=2e-2, weight_decay=1e-5),
+        mask_type='entmax',
+        scheduler_params=dict(mode="min",
+                              patience=5,
+                              min_lr=1e-5,
+                              factor=0.9,),
+        scheduler_fn=torch.optim.lr_scheduler.ReduceLROnPlateau,
+        verbose=10,
     )
     
     tb_reg.fit(X_train,Y_train,
-          eval_set=[(X_train, Y_train), (X_val, Y_val)],
-          eval_name=['train', 'valid'],
-          eval_metric=['rmse'],
-          max_epochs=10,
-          batch_size=32, drop_last=False)
+        eval_set=[(X_train, Y_train), (X_val, Y_val)],
+        eval_name=['train', 'valid'],
+        eval_metric=['rmse'],
+        max_epochs=100,
+        patience=10, batch_size=1024, virtual_batch_size=128,
+        drop_last=False
+    )
     
     return tb_reg
 
 def predict(tb_cls, X_test, y_test):
     # Test model and generate prediction
     return tb_cls.predict(X_test)
-
-def load_future_sales_dataset():
-    data_path = os.path.join('kaggle', 'input', 'competitive-data-science-predict-future-sales')
-    sales_train_df=pd.read_csv(os.path.join(data_path, 'sales_train.csv'))
-    
-    sales_train_df_groupby=sales_train_df.groupby(['shop_id','item_id','date_block_num'])['item_cnt_day'].sum().reset_index()    
-    sales_train_df_groupby_sorted = sales_train_df_groupby.sort_values(by=['date_block_num'])
-
-    X = sales_train_df_groupby_sorted.drop(columns=['item_cnt_day','date_block_num'])#['date_block_num','shop_id']  # values converts it into a numpy array
-    Y = sales_train_df_groupby_sorted['item_cnt_day']  # -1 means that calculate the dimension of rows, but have 1 column
-
-    X =  X.to_numpy()
-    Y = Y.to_numpy()
-    
-    Y = Y.flatten().reshape(-1, 1)
-
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-
-    return X_train, y_train, X_test, y_test
 
 def run_tab_net_for_future_sales():
     X, Y, X_test, Y_test = load_future_sales_dataset()
@@ -72,7 +62,7 @@ def run_tab_net_for_future_sales():
         tb_reg = train(X_train, Y_train, X_val, Y_val)
         CV_score_array.append(tb_reg.best_cost)
 
-        save_model('future_sales_predict_' + str(i), tb_reg)
+        save_model(os.path.join('future_sales', 'model_' + str(i)), tb_reg)
 
     print(CV_score_array)
 
